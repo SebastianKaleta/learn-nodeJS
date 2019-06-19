119c27123108f9237c28fb693cda3e124ab61c06
Sebastian <sebastian.m.kaleta@gmail.com>
2019-06-19 21:03:12 +0200

    module file system and simple examples

--- moduły/5-moduł_file_system/index.js
+++ moduły/5-moduł_file_system/index.js
@@ -25,7 +25,35 @@ const fs = require("fs");
 
 // console.log(fs.readdirSync("./"));
 
-fs.readdir("./", (err, files) => {
-  console.log("Błąd: ", err);
-  console.log("Zawartość: ", files);
+// fs.readdir("./", (err, files) => {
+//   if(err)return console.log("Błąd: ", err);
+//   console.log("Zawartość: ", files);
+// });
+
+// READFILE
+
+// asynchronicznie
+fs.readFile("names.txt", "utf-8", (err, data) => {
+  if (err) throw Error(err);
+  // używając throw zatrzyma się program a nie tylko przerwie funkcje
+  console.log(data);
 });
+
+// synchronicznie
+// Korzystając z sycnchronicznej metody najczesciej prypisujemy wynik do czegos
+
+// try {
+//   console.log(fs.readFileSync("names.txt", "utf8"));
+// } catch (err) {
+//   console.log(err);
+// }
+
+let names = "";
+
+try {
+  names = fs.readFileSync("names.txt", "utf8");
+} catch (err) {
+  // console.log(err);
+  names = false;
+}
+console.log(names);
